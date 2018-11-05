5b1a088590f8e4786d10fd44546a6dde82660c92
B533P-22\533 <awd>
2018-11-05 16:34:41 -0500

    Multiplayer semi fonctionnel

---
+++ Pod_Shooter/Assets/PlayerLife.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerLife : NetworkBehaviour {
+    public RectTransform healthbar;
+    public const int maxlife = 100;
+    private NetworkStartPosition[] spawnPoints;
+
+    [SyncVar(hook = "updateLife")]
+    int currentLife = maxlife;
+
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer)
+        {
+            spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void takeDamage(int damage)
+    {
+        if (isServer)
+        {
+            currentLife -= damage;
+            if (currentLife <= 0)
+            {
+                currentLife = 0;
+                Die();
+            }
+        }
+    }
+
+    private void updateLife(int newLife)
+    {
+        healthbar.sizeDelta = new Vector2(newLife,healthbar.sizeDelta.y);
+    }
+
+    private void Die()
+    {
+        RpcReSpawn();
+    }
+
+    [ClientRpc]
+    void RpcReSpawn()
+    {
+        if (isLocalPlayer)
+        {
+            currentLife = maxlife;
+            Vector3 spawnPoint = Vector3.zero;
+            if (spawnPoints != null && spawnPoints.Length > 0)
+            {
+                spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+            }
+            transform.position = spawnPoint;
+        }
+    }
+}
---
+++ Pod_Shooter/Assets/Script/PlayerDamage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class PlayerDamage : NetworkBehaviour, Damagable {
+    PlayerLife playerLife;
+
+    public void DealDamage(int damage)
+    {
+        playerLife.takeDamage(damage);
+    }
+	// Use this for initialization
+	void Start () {
+        playerLife = GetComponent<PlayerLife>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
